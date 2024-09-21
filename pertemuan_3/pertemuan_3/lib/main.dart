a52edc89411f10ce8ec1032db239bb63dcd63481
Kuuhaku456 <manikfilip03@gmail.com>
2024-09-21 23:20:02 +0800

    penambahan fitur pertemuan 3

--- pertemuan_3/pertemuan_3/lib/main.dart
+++ pertemuan_3/pertemuan_3/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Container(
+          SizedBox(
             width: 100,
             height: 77,
             child: Row(
@@ -69,9 +70,183 @@ class MyHomePage extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.grey[300],
                     borderRadius: BorderRadius.circular(50),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/dandadan.jpeg"),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 170,
+            margin: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: const Color(0xFFFFD18E),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 110,
+                  height: 170,
+                  margin: const EdgeInsets.fromLTRB(20, 5, 0, 5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/dokter.jpeg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 165,
+                  height: 170,
+                  margin: const EdgeInsets.fromLTRB(5, 5, 10, 5),
+                  padding: const EdgeInsets.only(left: 5, right: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 25),
+                      Text(
+                        "How do you fell?",
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          "Fill out your medical\ncard right now",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 11,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 165,
+                        height: 40,
+                        margin: const EdgeInsets.only(top: 15),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFF7EE2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Get Started",
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 70,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+            decoration: BoxDecoration(
+              color: const Color(0xFFFFD18E),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Icon(
+                  Icons.search,
+                  size: 40,
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    "How can we help you?",
+                    style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 85,
+            margin: const EdgeInsets.only(top: 10, bottom: 10),
+            padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Center(
+              child: ListView(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 110,
+                    margin: const EdgeInsets.only(right: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: FaIcon(
+                            FontAwesomeIcons.tooth,
+                            size: 30,
+                          ),
+                        ),
+                        Text(
+                          "Dentist",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 110,
+                    margin: const EdgeInsets.only(right: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  Container(
+                    width: 110,
+                    margin: const EdgeInsets.only(right: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  Container(
+                    width: 110,
+                    margin: const EdgeInsets.only(right: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
